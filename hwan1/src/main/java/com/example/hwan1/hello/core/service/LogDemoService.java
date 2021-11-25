ca846b74bb2493b9a58454d3e76d4479e9a5594f
Dongle <dongle@r2v.io>
2021-11-25 16:55:49 +0900

    feat: ObjectProvider 사용하여 request 스코프시에 문제를 해결

--- hwan1/src/main/java/com/example/hwan1/hello/core/service/LogDemoService.java
+++ hwan1/src/main/java/com/example/hwan1/hello/core/service/LogDemoService.java
@@ -2,15 +2,17 @@ package com.example.hwan1.hello.core.service;
 
 import com.example.hwan1.hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void log(String testId) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + testId);
     }
 }
--- hwan1/src/main/java/com/example/hwan1/hello/core/web/LogDemoController.java
+++ hwan1/src/main/java/com/example/hwan1/hello/core/web/LogDemoController.java
@@ -4,6 +4,7 @@ import com.example.hwan1.hello.core.common.MyLogger;
 import com.example.hwan1.hello.core.service.LogDemoService;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,12 +14,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestUrl = request.getRequestURI().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestUrl);
 
         myLogger.log("controller test");

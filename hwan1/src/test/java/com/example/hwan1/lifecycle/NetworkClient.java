092c4cda809d48837de70f6aa48f5f531bbbad6e
sk1737030 <sk1737030@naver.com>
2021-11-29 23:40:43 +0900

    feat: bean init destory 메소드명을 지정할 수 있다

--- hwan1/src/test/java/com/example/hwan1/lifecycle/NetworkClient.java
+++ hwan1/src/test/java/com/example/hwan1/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package com.example.hwan1.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -26,11 +29,13 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
     public void init() throws Exception {
         connect();
         call("초기화 연결 메세지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         disconnect();
     }

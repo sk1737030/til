7892b169ca4a1138378884a82dbb7f21b39b0a31
sk1737030 <sk1737030@naver.com>
2022-05-31 19:36:08 +0900

    feat: kafka shutdown test

---
+++ spring-kafka-graceful-shutdown/src/main/java/com/example/springkafkagracefulshutdown/SpringKafkaGracefulShutdownApplication.java
@@ -0,0 +1,55 @@
+package com.example.springkafkagracefulshutdown;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class SpringKafkaGracefulShutdownApplication implements CommandLineRunner {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaGracefulShutdownApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        for (int i = 0; i < 9; i++) {
+            this.kafkaTemplate.send("myTopic", "foo" + i);
+        }
+    }
+
+    @Bean
+    public KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        return new KafkaAdmin(configs);
+    }
+
+
+    @Bean
+    public KafkaAdmin.NewTopics topic() {
+        return new KafkaAdmin.NewTopics(TopicBuilder.name("myTopic")
+            .partitions(3)
+            .replicas(1)
+            .build());
+    }
+
+    @KafkaListener(topics = "myTopic", id = "my.group.id", concurrency = "1")
+    void listenTopic() {
+
+    }
+
+
+}
---
+++ spring-kafka-graceful-shutdown/src/test/java/com/example/springkafkagracefulshutdown/SpringKafkaGracefulShutdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkagracefulshutdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaGracefulShutdownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

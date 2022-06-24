83692756e8987c2039485fac346269335f3859cf
Dongle <sk1737030@naver.com>
2022-06-24 16:17:06 +0900

    feat: send money  validator 추가

--- clean-architecture/src/main/java/com/clean/architecture/hexagonal/account/application/port/in/SendMoneyCommand.java
+++ clean-architecture/src/main/java/com/clean/architecture/hexagonal/account/application/port/in/SendMoneyCommand.java
@@ -1,5 +1,35 @@
 package com.clean.architecture.hexagonal.account.application.port.in;
 
-public class SendMoneyCommand {
+import com.clean.architecture.hexagonal.account.common.SelfValidating;
+import com.clean.architecture.hexagonal.account.domain.Account;
+import com.clean.architecture.hexagonal.account.domain.Money;
+import com.sun.istack.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import static java.util.Objects.requireNonNull;
+
+public class SendMoneyCommand extends SelfValidating<SendMoneyCommand> {
+
+    @NotNull
+    private final Account.AccountId sourceAccountId;
+    @NotNull
+    private final Account.AccountId targetAccountId;
+    @NotNull
+    private Money money;
+
+    public SendMoneyCommand(Account.AccountId sourceAccountId, Account.AccountId targetAccountId, Money money) {
+        this.sourceAccountId = sourceAccountId;
+        this.targetAccountId = targetAccountId;
+        this.money = money;
+        requireGreaterThan(money);
+        this.validateSelf();
+    }
+
+    private void requireGreaterThan(Money money) {
+        if (!money.isPositive()) {
+            throw new IllegalArgumentException();
+        }
+    }
 
 }

1692dd84f30bf780acb9d9f18ab23332a949247b
Dongle <sk1737030@naver.com>
2022-07-22 10:34:10 +0900

    feat(x-unit): xunit 1단계

--- my-xunit/src/main/java/WasRun.java
+++ my-xunit/src/main/java/WasRun.java
@@ -1,9 +1,26 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class WasRun {
 
     public boolean wasRun;
+    private final String name;
+
+    public WasRun(String name) {
+        this.name = name;
+    }
 
     public void testMethod() {
         wasRun = true;
+    }
+
+    public void run() {
+        try {
+            Method method = getClass().getMethod(name);
+            method.invoke(this);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- my-xunit/src/main/java/XunitTest.java
+++ my-xunit/src/main/java/XunitTest.java
@@ -1,11 +1,10 @@
 public class XunitTest {
 
     public static void main(String[] args) {
-        WasRun wasRun = new WasRun();
+        WasRun wasRun = new WasRun("testMethod");
         System.out.println(wasRun.wasRun); // false
         wasRun.testMethod();
         System.out.println(wasRun.wasRun); // true
-
     }
 
 }

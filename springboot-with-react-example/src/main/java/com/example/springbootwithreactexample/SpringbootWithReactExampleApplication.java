7f3e511d5abf9371e99f45d735790d932927b6a7
dongho.geum <sk1737030@naver.com>
2022-10-07 18:17:23 +0900

    init: React with Spring boot Hello World

---
+++ springboot-with-react-example/src/main/java/com/example/springbootwithreactexample/SpringbootWithReactExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithreactexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithReactExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWithReactExampleApplication.class, args);
+    }
+
+}
---
+++ springboot-with-react-example/src/main/java/com/example/springbootwithreactexample/presentation/api/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.springbootwithreactexample.presentation.api;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public List<String> test() {
+        return Arrays.asList("안녕하세요", "Hello");
+    }
+
+}

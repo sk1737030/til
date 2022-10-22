f948662bef754d93b56899e0773f94eb8570d856
dongle <sk1737030@naver.com>
2022-10-22 20:34:02 +0900

    chore: 이동 목록에 버튼클릭시 하이라이트 되게 수정

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -87,7 +87,7 @@ class Game extends React.Component {
             const desc = move ? 'Go to move #' + move : 'Go to game start';
             return (
                 <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    <button className={"move-button"} onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             )
         });

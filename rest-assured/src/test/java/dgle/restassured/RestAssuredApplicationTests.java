01655bbb9333d889b7fc36506b9c2c85c8245e71
dongle <sk1737030@naver.com>
2023-03-25 01:09:14 +0900

    feat: restassured 사용

--- rest-assured/src/test/java/dgle/restassured/RestAssuredApplicationTests.java
+++ rest-assured/src/test/java/dgle/restassured/RestAssuredApplicationTests.java
@@ -2,9 +2,12 @@ package dgle.restassured;
 
 import dgle.restassured.presentation.OrderController;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -12,7 +15,7 @@ import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RestAssuredApplicationTests {
 
   protected static RequestSpecification REQUEST_SPEC;
@@ -20,12 +23,20 @@ class RestAssuredApplicationTests {
   @LocalServerPort
   private Integer port;
 
+  @BeforeEach
+  public void setRequestSpec() {
+    RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+    reqBuilder.setContentType(ContentType.JSON);
+    reqBuilder.setPort(port);
+    REQUEST_SPEC = reqBuilder.build();
+  }
+
   @Test
   void getOrder() {
     ExtractableResponse<Response> actualResponse =
         RestAssured.
             given().
-              /*spec(REQUEST_SPEC).*/log().all().
+              spec(REQUEST_SPEC).log().all().
             when().
               get("/order/{orderId}", 1L).
             then().
